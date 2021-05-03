43764fcda77c2d1f5184dd2bc4d62075c211b477
mas.ked.rin <mas.ked.rin@gmail.com>
2021-05-03 10:15:16 +0200

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Michał Ślusarczyk on 03/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
